df35f41a07181c9b7bd36ae8121d0ef1254c7465
Brian Porter <brian.john.porter@gmail.com>
2016-05-07 02:43:21 -0400

    File dump

---
+++ Assets/Scripts/Beat Detection/TestBeatDetection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBeatDetection : MonoBehaviour, AudioProcessor.AudioCallbacks {
+
+    private AudioProcessor audioProcessor;
+
+    void Awake() {
+        audioProcessor = Camera.main.GetComponent<AudioProcessor>();
+        audioProcessor.addAudioCallback(this);
+    }
+
+    void Start() {
+        //Select the instance of AudioProcessor and pass a reference
+        //to this object
+        //AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+    }
+
+
+    void Update() {
+
+    }
+
+    public void onOnbeatDetected() {
+        Debug.Log("Beat!!!");
+    }
+
+    public void onSpectrum(float[] spectrum) {
+
+    }
+}
